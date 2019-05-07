7b9fcde1084535ed2ba90c38b5135920a989dd94
Davindra Sutraban <razrsharp25@gmail.com>
2019-05-07 09:18:38 -0400

    Added pseudocode for example 1

---
+++ lesson_2/pseudo_code.rb
@@ -0,0 +1,21 @@
+# a method that returns the sum of two integers
+
+=begin casual
+Given two integers as arguments in a method
+
+Perform the addition of the two variables.
+
+(Because this is the last line of the method, the result of the
+addition operation will be returned, so there's no need to explicitly
+return something)
+=end
+=begin formal
+START
+
+argument1 + argument2
+
+END
+=end
+
+# a method that takes an array of strings, and returns a string that is all
+# those strings concatenated together
