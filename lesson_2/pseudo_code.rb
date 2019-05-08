c9aeaff58fec3788af13bbaa7a8b79505f42b1ea
Davindra Sutraban <razrsharp25@gmail.com>
2019-05-08 09:43:16 -0400

    Added pseudo-code for rest of exmaples

--- lesson_2/pseudo_code.rb
+++ lesson_2/pseudo_code.rb
@@ -19,3 +19,51 @@ END
 
 # a method that takes an array of strings, and returns a string that is all
 # those strings concatenated together
+
+=begin casual
+Given array of strings as arguments for a method
+
+Create a variable that will hold the final string
+
+Go through the array one by one
+  - pop the element
+  - add the popped element to the string using <<
+
+Return the final string variable
+=end
+=begin formal
+START
+
+SET final = ""
+
+WHILE array !empty
+  SET final = array.pop
+
+PRINT final
+
+END
+=end
+
+# a method that takes an arr of ints, returns a new array with every other element
+=begin casual
+Given an array of integers
+
+Create an new empty array
+Starting with index 0
+  - skip if index is odd
+  - newarr[index] == currentarray[index]
+
+return newarr
+=end
+=begin formal
+
+Given an array of ints
+START
+SET newarr = []
+
+FOR EACH arr index, value
+  NEXT IF index is odd
+  SET newarr[index] == value
+
+RETURN newarr
+=end
