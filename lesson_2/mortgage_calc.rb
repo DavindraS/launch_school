9c38522ef1c3bee0b94936300f37f9bbc3d98c81
Davindra Sutraban <razrsharp25@gmail.com>
2019-06-18 19:22:37 -0400

    Updated exercise solution with basic input correction

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -1,14 +1,41 @@
-Kernel.puts("What is the loan amount?")
-loan_total = Kernel.gets().chomp().to_f()
-p loan_total
-Kernel.puts("What is the APR? Please state in percentage (ex. if it's 5% enter 5)")
-apr = Kernel.gets().chomp().to_f()
-p apr
-Kernel.puts("What is the length of the loan in months? (ex. if 2 years enter 24)")
-loan_length = Kernel.gets().chomp().to_i()
-p loan_length
-monthly_interest_rate = (apr / 12) / 100
-p monthly_interest_rate
+def prompt(message)
+  Kernel.puts("->#{message}")
+end
+
+def is_valid_number(num)
+  if !num.is_a?(Float) || num <= 0
+    prompt("Please enter a valid number")
+    return false
+  else
+    return true
+  end
+end
+
+prompt("Welcome to Loan Calculator!")
+prompt("What is the loan amount?")
+prompt("Please enter as a the full dollar amount including cents, ex. 123.00")
+loan_total = ''
+loop do
+  loan_total = Kernel.gets().chomp().to_f()
+  break if is_valid_number(loan_total)
+end
+
+prompt("What is the APR?")
+prompt("Please state the number as a percent (ex. if it's 5% enter 5)")
+apr = ''
+loop do
+  apr = Kernel.gets().chomp().to_f()
+  break if is_valid_number(apr)
+end
+
+prompt("What is the length of the loan in months?")
+prompt("Ex. if length is 2 years enter 24")
+loan_length = ''
+loop do
+  loan_length = Kernel.gets().chomp().to_f()
+  break if is_valid_number(loan_length)
+end
+
+monthly_interest_rate = ((apr / 12) / 100)
 monthly_payment = loan_total * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-loan_length)))
-p monthly_payment
-Kernel.puts("Your monthly payment is going to be $#{monthly_payment}")
+prompt("Your monthly payment is going to be $#{monthly_payment.round(2)}.")
