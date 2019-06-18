bf3866cf739bcf0d00bc2613f979dd7b4e19a42b
Davindra Sutraban <razrsharp25@gmail.com>
2019-06-18 19:30:24 -0400

    Added rubocop suggestions

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -2,12 +2,12 @@ def prompt(message)
   Kernel.puts("->#{message}")
 end
 
-def is_valid_number(num)
+def valid_number?(num)
   if !num.is_a?(Float) || num <= 0
     prompt("Please enter a valid number")
-    return false
+    false
   else
-    return true
+    true
   end
 end
 
@@ -17,7 +17,7 @@ prompt("Please enter as a the full dollar amount including cents, ex. 123.00")
 loan_total = ''
 loop do
   loan_total = Kernel.gets().chomp().to_f()
-  break if is_valid_number(loan_total)
+  break if valid_number?(loan_total)
 end
 
 prompt("What is the APR?")
@@ -25,7 +25,7 @@ prompt("Please state the number as a percent (ex. if it's 5% enter 5)")
 apr = ''
 loop do
   apr = Kernel.gets().chomp().to_f()
-  break if is_valid_number(apr)
+  break if valid_number?(apr)
 end
 
 prompt("What is the length of the loan in months?")
@@ -33,9 +33,9 @@ prompt("Ex. if length is 2 years enter 24")
 loan_length = ''
 loop do
   loan_length = Kernel.gets().chomp().to_f()
-  break if is_valid_number(loan_length)
+  break if valid_number?(loan_length)
 end
 
 monthly_interest_rate = ((apr / 12) / 100)
-monthly_payment = loan_total * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**(-loan_length)))
+monthly_payment = (loan_total * (monthly_interest_rate / (1 - (1 + monthly_interest_rate)**-loan_length)))
 prompt("Your monthly payment is going to be $#{monthly_payment.round(2)}.")
