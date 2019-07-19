5fef1a66095735e04e4e8bce3c23f705bf0120a2
Davindra Sutraban <razrsharp25@gmail.com>
2019-07-18 20:12:05 -0400

    Added answer for question 1

---
+++ lesson_3/easy1.rb
@@ -0,0 +1,7 @@
+# Question 1
+=begin
+  I would expect the code to print out the numbers in the array [1, 2, 2, 3]
+  According to the docs, Array#uniq would return a new array with
+  duplicate values removed. That means that it doesn't modify the original
+  array.
+=end
