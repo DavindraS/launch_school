3d3ef15415a50dd13b31c318fee5b4fb6951088a
Davindra Sutraban <razrsharp25@gmail.com>
2019-07-18 20:22:53 -0400

    Added answers for question 2

--- lesson_3/easy1.rb
+++ lesson_3/easy1.rb
@@ -5,3 +5,16 @@
   duplicate values removed. That means that it doesn't modify the original
   array.
 =end
+
+# Question 2
+=begin
+  ! at the end of a method usually means that it will modify the object calling
+  ! also means NOT or negate, so != means not equal
+  ? after a method name usually means that the method returns a boolean
+  1. != means not equals, and its used with comparisons and branching
+  2. ! before something means the opposite of what the thing is
+  3. ! after a method means that the method changes the object that called it
+  4. ? before something: I'm not sure this is allowed
+  5. ? after something means that the thing will return a boolean
+  6. !! means not not. ex. !!true = true. !true = false, so !false = true
+=end
