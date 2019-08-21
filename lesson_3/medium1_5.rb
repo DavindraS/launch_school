6828004fe2277b4a79c75734b83af3bcbd47cc01
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-21 13:59:25 -0400

    Added a solution for question 5

---
+++ lesson_3/medium1_5.rb
@@ -0,0 +1,34 @@
+# What's wrong with the code and how would you fix it
+=begin
+limit = 15
+
+def fib(first_num, second_num)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1)
+puts "result is #{result}"
+=end
+
+# The limit variable is out of scope of the fib function, so it has no idea
+# what limit is referring to. To solve this, we can add a method parameter
+# and pass in the limit as an argument.
+
+limit = 15
+
+def fib(first_num, second_num, limit)
+  while first_num + second_num < limit
+    sum = first_num + second_num
+    first_num = second_num
+    second_num = sum
+  end
+  sum
+end
+
+result = fib(0, 1, limit)
+puts "result is #{result}"
