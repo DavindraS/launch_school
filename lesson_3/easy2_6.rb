516a2b77fde2e8716e485ec2fbb32c51e0461a0d
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-21 09:16:44 -0400

    Added a solution for question 6

---
+++ lesson_3/easy2_6.rb
@@ -0,0 +1,8 @@
+# Add the family pet Dino to our usual array
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones.push("Dino")
+# flintstones << "Dino" is the official solution
+
+p flintstones
