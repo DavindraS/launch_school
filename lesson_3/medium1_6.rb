d3602297613cf184dd1d3dfcdf589aa5e20bdbe1
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-21 14:01:55 -0400

    Added an answer for question 6

---
+++ lesson_3/medium1_6.rb
@@ -0,0 +1,3 @@
+# That code will return 42-8 or 34. The reason is because the mess_with_it
+# function does not change the answer variable, so when we go to p answer, its
+# referring to the original answer variable 42.
