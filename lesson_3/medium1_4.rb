96645ca722cef171390faf25ddf747c88348e432
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-21 13:54:14 -0400

    Added an answer for question 4

---
+++ lesson_3/medium1_4.rb
@@ -0,0 +1,5 @@
+=begin
+  The difference between the two is that + will be adding the two arrays,
+  creating a third array, while << will modify the original array. So
+  + will not modify the input array, but << will modify the input array.
+=end
