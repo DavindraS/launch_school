a3619033cb22bdfad0efaf4d6ae71fa143a64b2e
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-20 21:41:50 -0400

    Added an answer to question 4

--- lesson_3/easy1.rb
+++ lesson_3/easy1.rb
@@ -24,3 +24,9 @@
   To replace the word important with urgent, I would use the following code:
   advice.gsub!('important, 'urgent')
 =end
+
+# Question 4
+=begin
+  numbers.delete_at(1) will remove the element at index 1 in this case, 2
+  numbers.delete(1) will remove all the number 1's in the array
+=end
