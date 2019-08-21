8cd0f5c7905d46bf69049e3b48d33a36a296380b
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-20 22:08:23 -0400

    Added an answer for question 7

--- lesson_3/easy1.rb
+++ lesson_3/easy1.rb
@@ -50,3 +50,10 @@
   1. famous_words.insert(0, "Four score and ")
   2. famous_words.prepend("Four score and ")
 =end
+
+# Question 7
+=begin
+  The answer is 42. The docs say that Kernel#eval evaluates the ruby expression
+  in string. So when we call that on how_deep, number is evaluated to the integer
+  2, and we add 8 5 times which gets us to 42. 
+=end
