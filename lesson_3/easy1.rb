756b8ce186648c3b058951fd665b22f16319f498
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-20 21:52:02 -0400

    Added an explanation for my question 5 answer

--- lesson_3/easy1.rb
+++ lesson_3/easy1.rb
@@ -34,6 +34,15 @@
 # Question 5
 =begin
   Programmatically determine if 42 lies between 10 and 100 (use range)
-  (10..100).include?(42). This is my original answer
-  (10..100).cover?(42)
+  (10..100).include?(42) # This is my original answer
+  (10..100).cover?(42) # official answer
+  There is a difference between these two is that cover compares the input
+  and checks whether its lower than the end and higher than the beginning,
+  while include iterates through the entire range to find whether the input
+  is one of them. Cover seems faster for this case.
+=end
+
+# Question 6
+=begin
+
 =end
