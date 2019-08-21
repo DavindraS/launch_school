94e77765558a643c1050d2762bfdc4e38e534b6d
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-21 09:01:49 -0400

    Added a solution for question 2

---
+++ lesson_3/easy2_2.rb
@@ -0,0 +1,15 @@
+munsters_description = "The Munsters are creepy in a good way."
+
+=begin
+convert the string into the following:
+
+"tHE mUNSTERS ARE CREEPY IN A GOOD WAY."
+"The munsters are creepy in a good way."
+"the munsters are creepy in a good way."
+"THE MUNSTERS ARE CREEPY IN A GOOD WAY."
+=end
+
+puts munsters_description.swapcase!
+puts munsters_description.capitalize!
+puts munsters_description.downcase!
+puts munsters_description.upcase!
