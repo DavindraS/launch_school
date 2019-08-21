b7b43ebb92e612d8c73cd16f8cd558985a35cca4
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-20 22:18:02 -0400

    Added a solution for question 9

--- lesson_3/easy1.rb
+++ lesson_3/easy1.rb
@@ -63,3 +63,12 @@
   Un-nest the array ["Fred", "Wilma", ["Barney", "Betty"], ["BamBam", "Pebbles"]]
   flintstones.flatten!
 =end
+
+# Question 9
+=begin
+  Turn this hash into an array with Barney's name and number
+  flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+  flintstones.keep_if{|k, v| k == "Barney"}.to_a.flatten! # my answer
+
+  flintstones.assoc("Barney") # official and better answer
+=end
