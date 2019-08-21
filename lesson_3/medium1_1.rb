fac86c8d11f79a003953651cfa805c5469f477da
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-21 12:59:25 -0400

    Added an explanation to my solution

--- lesson_3/medium1_1.rb
+++ lesson_3/medium1_1.rb
@@ -4,5 +4,6 @@
 str = "The Flintstones Rock!"
 
 10.times{ |i| puts str.rjust(i+str.length, " ")} # Original solution
-
+# Using rjust only adds the padding if the length of the string is more than i
+# so in this case, to always make it greater than i, I just added the length+i
 10.times { |number| puts (" " * number) + "The Flintstones Rock!" } # Official
