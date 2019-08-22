86249648efac89276793d2de4959907e11f44ca3
Davindra Sutraban <razrsharp25@gmail.com>
2019-08-21 20:07:30 -0400

    Added an answer for question 7

---
+++ lesson_3/medium1_7.rb
@@ -0,0 +1,6 @@
+=begin
+  Yes the family data did get ransacked.
+
+  What the code did was take each value hash individually, and added 42 to the
+  age and changed the gender to other. It worked on the munsters hash directly.
+=end
